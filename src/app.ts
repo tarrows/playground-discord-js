bdf6c790b6923a9a8ccc7cddacd492c657bf7c00
tarrows <c10t@fa.ke>
2020-06-20 23:45:33 +0900

    create template

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('hello ts + node!')
+import Discord from 'discord.js';
+import fs from 'fs';
+
+import config from './config.json';
+
+const client = new Discord.Client();
+
+const generateOutputFile = (channel: Discord.Channel, member: Discord.GuildMember) => {
+  const fileName = `./recordings/${channel.id}-${member.id}-${Date.now()}.pcm`;
+  return fs.createWriteStream(fileName);
+}
+
+client.on('message', msg => {})
+
+client.login(config.token)
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client?.user?.tag}!`);
+})
